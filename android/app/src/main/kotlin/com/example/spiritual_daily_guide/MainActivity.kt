08fc10e1bec4a74248be716b69cf10c89fc604f5
Solomon <laleyesolomon@gmail.com>
2022-08-04 03:10:21 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/spiritual_daily_guide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spiritual_daily_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spiritual_daily_guide/presentations/homepage.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
